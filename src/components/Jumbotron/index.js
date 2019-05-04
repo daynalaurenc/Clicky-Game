fcf6bf03f5572982a632268421ff2543923a2ece
DLC <daynalaurenc@gmail.com>
2019-05-04 12:54:52 -0700

    logo styled

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function Jumbotron(props) {
     return (
         <div className="jumbotron text-center">
-            <h1>CLICKY GAME</h1>
-            <img src="./images"
-            <p className="by-line">GOLDEN STATE WARRIORS</p><hr/>
+            {/* <h1>CLICKY GAME</h1> */}
+            <img src="./images/logo.png" alt="" id="logo"/>
+            <p className="by-line">Click until all have been chosen;<br/> do not click same image twice!</p><hr/><br />
 
             <p className="currentScore">score: {props.score}</p>
             <p className="topScore">top score: {props.topScore}</p>
